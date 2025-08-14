e5771e6c9908a50d3b6226876c450c0e6e4dc2af
Anthony RUbio <rubionn27@gmail.com>
2025-08-14 11:11:52 -0500

    Add: seeder flights

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Flight;
 use Illuminate\Http\Request;
 
 class FlightController extends Controller
@@ -9,9 +10,24 @@ class FlightController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return \App\Models\Flight::orderBy('departure_at')->get();
+        $request->validate([
+            'origin' => ['nullable','string','max:3'],
+            'destination' => ['nullable','string','max:3'],
+            'date' => ['nullable', 'data'],
+            'page' => ['nullable','integer','min:1'],
+            'per_page' => ['nullable','integer','min:1','max:100']
+        ]);
+
+        $perPage = (int)($request->input('per_page',10));
+        $query = Flight::query()
+            ->when($request->origin, fn ($query,$value) => $query->where('origin', strtoupper($value)))
+            ->when($request->destination, fn ($query,$value) => $query->where('destination', strtoupper($value)))
+            ->when($request->date, fn ($query,$value) => $query->where('departure_at', $value))
+            ->orderBy('departure_at');
+
+        return $query->paginate($perPage)->appends($request->query());
     }
 
     /**
--- database/seeders/FlightSeeder.php
+++ database/seeders/FlightSeeder.php
@@ -19,6 +19,14 @@ class FlightSeeder extends Seeder
             ['KNC102','MDE','CTG','+1 day 14:00',90, 90,  280.00],
             ['KNC201','BOG','CLO','+2 day 07:30',110,110,300.00],
             ['KNC301','CLO','BOG','+3 day 18:45',100,100,320.00],
+            ['SNC301','CLO','BOG','+3 day 18:45',100,100,320.00],
+            ['FNC301','CLO','BOG','+3 day 18:45',100,100,320.00],
+            ['GNC301','CLO','BOG','+3 day 18:45',100,100,420.00],
+            ['KNC301','CLO','BOG','+3 day 18:45',100,100,620.00],
+            ['KNG301','CLO','BOG','+3 day 18:45',100,100,120.00],
+            ['KJC301','CLO','BOG','+3 day 18:45',100,100,50.00],
+            ['ENC301','CLO','BOG','+3 day 18:45',100,100,540.00],
+            ['ANC301','CLO','BOG','+3 day 18:45',100,100,410.00],
         ];
         foreach ($rows as [$code,$o,$d,$when,$total,$avail,$price]) {
             Flight::updateOrCreate(
